d5d3ad8644c4993b59e43e142b469ae7316dc466
Garik <garfild_2003@mail.ru>
2024-04-26 22:24:58 +0300

    Added view for war

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+
+  end
+
+  def warehouses
     @team1 = Character.where(team: 1)
     @team2 = Character.where(team: 2)
   end
@@ -14,28 +18,49 @@ class HomeController < ApplicationController
 
   def create_knight
     Character.create(team: 1, unit: 'knight')
-    redirect_to root_path
+    redirect_to warehouses_path
   end
 
   def create_mage
     Character.create(team: 1, unit: 'mage')
-    redirect_to root_path
+    redirect_to warehouses_path
   end
 
   def create_medusa
     Character.create(team: 2, unit: 'medusa')
-    redirect_to root_path
+    redirect_to warehouses_path
   end
 
   def create_jinn
     Character.create(team: 2, unit: 'jinn')
-    redirect_to root_path
+    redirect_to warehouses_path
   end
 
   def destroy_jinn
     @character = Character.where(team: 2, unit: 'jinn').last
     @character&.destroy
 
-    redirect_to root_path
+    redirect_to warehouses_path
+  end
+
+  def destroy_medusa
+    @character = Character.where(team: 2, unit: 'medusa').last
+    @character&.destroy
+
+    redirect_to warehouses_path
+  end
+
+  def destroy_mage
+    @character = Character.where(team: 1, unit: 'mage').last
+    @character&.destroy
+
+    redirect_to warehouses_path
+  end
+
+  def destroy_knight
+    @character = Character.where(team: 1, unit: 'knight').last
+    @character&.destroy
+
+    redirect_to warehouses_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
    root "home#index"
 
+   get "/warehouses", to: "home#warehouses"
+
    get "/create_knight", to: "home#create_knight"
 
    get "/create_medusa", to: "home#create_medusa"
@@ -10,4 +12,10 @@ Rails.application.routes.draw do
    get "/create_jinn", to: "home#create_jinn"
 
    get "/destroy_jinn", to: "home#destroy_jinn"
+
+   get "/destroy_mage", to: "home#destroy_mage"
+
+   get "/destroy_medusa", to: "home#destroy_medusa"
+
+   get "/destroy_knight", to: "home#destroy_knight"
 end
