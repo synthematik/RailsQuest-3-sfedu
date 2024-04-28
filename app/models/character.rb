b347cb1b4f89b448fe46955c913dcd1d0f0c1823
Garik <garfild_2003@mail.ru>
2024-04-28 21:50:33 +0300

    Removed unnesessary code

--- app/models/character.rb
+++ app/models/character.rb
@@ -16,34 +16,4 @@ class Character < ApplicationRecord
       []
     end
   end
-
-  def normal_unit_ability
-    case unit
-    when 'mage'
-      "Огненный шар"
-    when 'knight'
-      "Удар мечом"
-    when 'medusa'
-      "Остолбеней"
-    when 'jinn'
-      "Шторм"
-    else
-      "Упс, что-то где-то почему-то пошло не так"
-    end
-  end
-
-  def ultimate_unit_ability
-    case unit
-    when 'mage'
-      "Супер огненный шар"
-    when 'knight'
-      "Супер удар мечом"
-    when 'medusa'
-      "Супер остолбеней"
-    when 'jinn'
-      "Супер шторм"
-    else
-      "Упс, что-то где-то почему-то пошло не так"
-    end
-  end
 end
